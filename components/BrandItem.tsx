1c81069ddd6d66334433fdf1ae431b7607a375fb
SamuelAMZ <samueldevpro09@gmail.com>
2024-09-11 23:35:02 +0000

    Fix: fetch brand svg data

--- components/BrandItem.tsx
+++ components/BrandItem.tsx
@@ -1,5 +1,6 @@
 import { PropsWithChildren } from "react";
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
+
 import ThemedText from "./ThemedText";
 
 type BrandType = {
@@ -24,17 +25,7 @@ const BrandItem = ({
         style={{ height: size, width: size, borderRadius: 70 }}
         className={` border border-[#D0D5DD] flex justify-center items-center`}
       >
-        <Image
-          source={
-            brand?.logo
-              ? { uri: brand.logo }
-              : require("@/assets/images/bmw.png")
-          }
-          style={{
-            height: 45,
-            width: 45,
-          }}
-        />
+        <BrandLogo brand={brand} />
       </TouchableOpacity>
       <ThemedText className="font-semibold">{brand?.name}</ThemedText>
     </View>
@@ -42,3 +33,50 @@ const BrandItem = ({
 };
 
 export default BrandItem;
+
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator } from "react-native";
+import { SvgXml } from "react-native-svg";
+
+const BrandLogo = ({ brand }: { brand: any }) => {
+  const [svgData, setSvgData] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    try {
+      if (brand?.logo && brand.logo.endsWith(".svg")) {
+        // Fetch the SVG content from the remote URL using fetch API
+        fetch(brand.logo)
+          // Get the SVG text
+          .then((response) => response.text())
+          .then((data) => {
+            setSvgData(data);
+            setLoading(false);
+          })
+          .catch((error) => {
+            console.error("Error fetching SVG:", error);
+            setLoading(false);
+          });
+      }
+    } catch (e) {
+      console.log(e, "error fetching svg content");
+    }
+  }, [brand]);
+
+  if (loading) {
+    return <ActivityIndicator />;
+  }
+
+  return (
+    <View>
+      {svgData ? (
+        <SvgXml xml={svgData} width="45" height="45" />
+      ) : (
+        <Image
+          source={require("@/assets/images/bmw.png")}
+          style={{ height: 45, width: 45 }}
+        />
+      )}
+    </View>
+  );
+};
